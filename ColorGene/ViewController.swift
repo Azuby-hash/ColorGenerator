e643532c13df577a7188a289e2b26ef91fc77d18
Azuby <haivip123vp@gmail.com>
2022-09-13 21:08:48 +0700

    Initial Commit

---
+++ ColorGene/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ColorGene
+//
+//  Created by Hai Le on 13/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
